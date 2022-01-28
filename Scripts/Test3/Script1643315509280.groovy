76d91dc1817d9925bc9ee668a869a6cc1f1568ac
adoni <adoni@LAPTOP-L8MLT6QA>
2022-01-28 10:31:12 -0400

    Agregando validación a Test3

--- Scripts/Test3/Script1643315509280.groovy
+++ Scripts/Test3/Script1643315509280.groovy
@@ -21,15 +21,34 @@ WebUI.openBrowser('')
 
 WebUI.navigateToUrl(url)
 
+//Validando elemento clickable
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Amazon.com. Gasta menos. Sonre ms/a_Hola, Identifcate  Cuenta y Listas'), 0)
+
+WebUI.verifyElementClickable(findTestObject('Object Repository/Page_Amazon.com. Gasta menos. Sonre ms/a_Hola, Identifcate  Cuenta y Listas'))
+
 WebUI.click(findTestObject('Object Repository/Page_Amazon.com. Gasta menos. Sonre ms/a_Hola, Identifcate  Cuenta y Listas'))
+
+//Validando input username
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Direccin de correo electrnico o nmero_cfb2c0'), 0)
 
 WebUI.setText(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Direccin de correo electrnico o nmero_cfb2c0'), 
     username)
 
+//Validando boton username
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Escriba su correo electrnico o su nme_4466f0'), 0)
+
+WebUI.verifyElementClickable(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Escriba su correo electrnico o su nme_4466f0'))
+
 WebUI.click(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Escriba su correo electrnico o su nme_4466f0'))
+
+//Validando boton password
+WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Introduce tu contrasea_signInSubmit'), 0)
+
+WebUI.verifyElementClickable(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Introduce tu contrasea_signInSubmit'))
 
 WebUI.click(findTestObject('Object Repository/Page_Amazon Iniciar sesin/input_Introduce tu contrasea_signInSubmit'))
 
+//Validando elemento
 WebUI.verifyElementPresent(findTestObject('Object Repository/Page_Amazon Iniciar sesin/div_Introduce tu contrasea'), 0)
 
 WebUI.delay(5)
